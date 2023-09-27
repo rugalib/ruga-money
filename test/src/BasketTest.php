ca36895223eca15884032001c6cecc4554073c75
Roland Rusch <roland@rusch.lu>
2023-09-27 15:50:08 +0200

    #11 Change License to AGPL-3.0-only

--- test/src/BasketTest.php
+++ test/src/BasketTest.php
@@ -1,9 +1,12 @@
 <?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 declare(strict_types=1);
 
 namespace Ruga\Money\Test;
-
 
 use Ruga\Money\Basket;
 use Ruga\Money\Basket\BasketableInterface;
--- test/src/PriceTest.php
+++ test/src/PriceTest.php
@@ -1,10 +1,13 @@
 <?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 declare(strict_types=1);
 
 namespace Ruga\Money\Test;
 
-use Laminas\ServiceManager\ServiceManager;
 use Ruga\Money\Amount;
 use Ruga\Money\Price;
 
